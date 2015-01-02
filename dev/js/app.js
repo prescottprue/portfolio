f769af962f2d1826561e7eb19bb276683e24ad45
Scott Prue <sprue.dev@gmail.com>
2015-01-02 03:09:57 -0500

    Styling added to caption box (black transparent with white lettering). Misc changed to hearthub project.

--- dev/js/app.js
+++ dev/js/app.js
@@ -9,20 +9,29 @@ angular.module('portfolioApp', [])
           for (var i = 0; i < scope.projects.length; i++) {
             var section = angular.element("<section>");
             var steps = scope.projects[i].pages;
-            if(typeof steps == 'undefined'){
-              var content = angular.element(scope.projects[i].intro);
+            // Project doesn't contain pages
+            if(!scope.projects[i].hasOwnProperty('pages')){
+              var content = angular.element(scope.projects[i].content);
               section.append(content);
             }
+            //if there is only one step
             else if (steps.length == 1) {
               var content = angular.element("<h2>").html(scope.projects[i].name);
               section.append(content);
             } else {
               for (var j = 0; j < steps.length; j++) {
                 var elementHtmlString = '<section class="reveal_section">'
+                var subSection = null;
                 if(steps[j].hasOwnProperty('image')){
-                  elementHtmlString = '<section class="reveal_section" data-background="'+ steps[j].image.url+'">'
+                  if(!steps[j].image.hasOwnProperty('style')){
+                    elementHtmlString = '<section class="reveal_section" data-background="'+ steps[j].image.url+'">'
+                    subSection = angular.element(elementHtmlString);
+                  } else {
+                    elementHtmlString = '<section class="reveal_section">'
+                    subSection = angular.element(elementHtmlString);
+                    subSection.append('<img style="'+steps[j].image.style+'" src="'+ steps[j].image.url +'">')
+                  }
                 }
-                var subSection = angular.element(elementHtmlString);
                 // var content = angular.element("<h1>").html(steps[j].caption);
                 // subSection.append(caption);
                 section.append(subSection);
