f61a5901df6676e0b86383c0ea0f8eb67bb5574e
Scott Prue <sprue.dev@gmail.com>
2015-01-23 23:12:50 -0500

    Comments and organization added to slideshow component.

--- dev/components/slideshow/main.js
+++ dev/components/slideshow/main.js
@@ -4,27 +4,22 @@ angular.module('slideshow', ['firebase'])
   console.log('App is running.');
 }])
 .controller('SlideshowCtrl', ['$rootScope','$scope', 'projects', function($rootScope, $scope, projects){
-  console.log('SlideshowCtrl', projects);
-     projects.$loaded().then(function(projectsArray){
-       console.log('projectsArray loaded', projectsArray);
-       $scope.projects = projectsArray;
-       console.log('$scope projects set', $scope.projects);
+  console.log('SlideshowCtrl');
+ $scope.projects = projects;
+ console.log('$scope projects set', $scope.projects);
 
-       // Slide change listener
-       Reveal.addEventListener( 'slidechanged', function( event ) {
-         // event.previousSlide, event.currentSlide, event.indexh, event.indexv
-         var projectData = $scope.projects[event.indexh];
-         var slideData = {project:projectData};
-         if(projectData.hasOwnProperty('pages') && projectData.pages.length){
-           slideData.slide = projectData.pages[event.indexv];
-         }
-         $scope.currentSlide = slideData;
-         console.log('currentSlide:', $scope.currentSlide);
-         $scope.$apply();
-       });
-
-    });
-
+ // Slide change listener
+ Reveal.addEventListener( 'slidechanged', function( event ) {
+   // event.previousSlide, event.currentSlide, event.indexh, event.indexv
+   var projectData = $scope.projects[event.indexh];
+   var slideData = {project:projectData};
+   if(projectData.hasOwnProperty('pages') && projectData.pages.length){
+     slideData.slide = projectData.pages[event.indexv];
+   }
+   $scope.currentSlide = slideData;
+   console.log('currentSlide:', $scope.currentSlide);
+   $scope.$apply();
+ });
 
   // Set first slide to home page
   $scope.currentSlide = {name:'Home', pages:[{caption:'Click site'}]};
@@ -75,31 +70,31 @@ angular.module('slideshow', ['firebase'])
                 elementString = '<section data-background="'+ projects[i].background +'">'; // Add background to element if it exists
               }
               var section = angular.element(elementString);   // Section element
-              var steps = projects[i].pages;                  // Pages param as steps var
+              var projectPages = projects[i].pages;                  // Pages param as projectPages var
               if(!projects[i].hasOwnProperty('pages')){       // Project doesn't contain pages
                 var content = angular.element(projects[i].content);    // Content element
                 section.append(content);                      // Content ELement to section
-              }
-              else if (steps.length == 1) {                   // Check if there is only one step
+              } else if (projectPages.length == 1) {                   // Check if there is only one step
                 var content = angular.element("<h2>").html(projects[i].name);      //Project Name element
                 section.append(content);                      // Project Name Element to section
               } else {
-                for (var j = 0; j < steps.length; j++) {      // Loop through pages
+                // More than one page exists
+                for (var j = 0; j < projectPages.length; j++) {      // Loop through pages
                   var elementHtmlString = '<section class="reveal_section">';
                   var subSection = null;
-                  if(steps[j].hasOwnProperty('image')){       //Check for image in page
-                    if(!steps[j].image.hasOwnProperty('style')){  //No Background styling
-                      elementHtmlString = '<section class="reveal_section" data-background="'+ steps[j].image.url+'">'//Background url from page
+                  if(projectPages[j].hasOwnProperty('image')){       //Check for image in page
+                    if(!projectPages[j].image.hasOwnProperty('style')){  //No Background styling
+                      elementHtmlString = '<section class="reveal_section" data-background="'+ projectPages[j].image.url+'">'//Background url from page
                       subSection = angular.element(elementHtmlString);
                     } else {                                  //Background styling exists
                       elementHtmlString = '<section class="reveal_section">';
                       subSection = angular.element(elementHtmlString);
-                      subSection.append('<img style="'+steps[j].image.style+'" src="'+ steps[j].image.url +'">'); //Append image to subsection
+                      subSection.append('<img style="'+projectPages[j].image.style+'" src="'+ projectPages[j].image.url +'">'); //Append image to subsection
                     }
                   }
                   // var content = angular.element("<h1>").html(steps[j].caption);
                   // subSection.append(caption);
-                  section.append(subSection);//Add subsection to section element
+                  section.append(subSection);               //Add subsection to section element
                 }
               }
               elem.append(section);                         //Append section to element
