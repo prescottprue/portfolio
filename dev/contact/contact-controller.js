8640456d4d963a84050a0873c5fb1c27f80f0a27
Scott Prue <prescottprue@gmail.com>
2015-07-13 02:45:42 -0400

    Made font HUGE. Added spacing and dividers to project sections. Contact page formatted and functioning.

--- dev/contact/contact-controller.js
+++ dev/contact/contact-controller.js
@@ -1,16 +1,35 @@
 angular.module('portfolioApp')
-  .controller('ContactCtrl', function($scope, $mdDialog, projectService){
+  .controller('ContactCtrl', function($scope, $mdDialog, projectService, ENV, $mdToast){
     console.log('Contact controller');
-    $scope.user = {
-      title: 'Developer',
-      email: 'ipsum@lorem.com',
-      firstName: '',
-      lastName: '' ,
-      company: 'Google' ,
-      address: '1600 Amphitheatre Pkwy' ,
-      city: 'Mountain View' ,
-      state: 'CA' ,
-      biography: 'Loves kittens, snowboarding, and can type at 130 WPM.\n\nAnd rumor has it she bouldered up Castle Craig!',
-      postalCode : '94043'
+    var ref = new Firebase(ENV.FBURL() + "/portfolio/messages");
+    $scope.toastPosition = {
+      bottom: false,
+      top: true,
+      left: false,
+      right: true
     };
+    $scope.getToastPosition = function() {
+      return Object.keys($scope.toastPosition)
+        .filter(function(pos) { return $scope.toastPosition[pos]; })
+        .join(' ');
+    };
+    $scope.showToast = function(msg) {
+      $mdToast.show(
+        $mdToast.simple()
+          .content(msg)
+          .position($scope.getToastPosition())
+          .hideDelay(3000)
+      );
+    };
+    $scope.sendMessage = function(){
+      if($scope.message && $scope.message.email && $scope.message.body){
+        ref.push({email:$scope.message.email, content:$scope.message.body}, function(err){
+          if(!err){
+            $scope.showToast('Message sent successfully!');
+          }
+        })
+
+      }
+    };
+
   });
